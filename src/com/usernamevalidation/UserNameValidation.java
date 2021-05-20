ed3d29d7e5dd9ffef995923351269500e0604bf9
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-20 21:41:36 +0530

    initial commit

---
+++ src/com/usernamevalidation/UserNameValidation.java
@@ -0,0 +1,10 @@
+package com.usernamevalidation;
+
+public class UserNameValidation {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the username validation program.");
+		
+	}
+	
+}
