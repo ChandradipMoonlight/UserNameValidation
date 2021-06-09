8461eb42c528b9ff38585839be71f7b8b7ec9e93
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-06-09 23:10:45 +0530

    Refactored: using lambda expression to validate user details.

--- src/main/java/com/userregistration/UserValidation.java
+++ src/main/java/com/userregistration/UserValidation.java
@@ -2,6 +2,10 @@ package com.userregistration;
 
 import java.util.regex.Pattern;
 
+@FunctionalInterface
+interface ValidateUsersDetails {
+    String validate(String input) throws UserValidationException;
+}
 public class UserValidation {
     /* Declaration of constants for regex pattern */
 
@@ -14,10 +18,10 @@ public class UserValidation {
     private static final String MOBILE_PATTERN = "^[9][1]\\s[6-9][0-9]{9}$";
     // regex pattern for password
     private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*" +
-                                                     "[@#$%^&-+=()]).{8,}$";
+                                                    "[@#$%^&-+=()]).{8,}$";
 
-    // Method to validate first name and las t name
-    public String validateName(String name) throws UserValidationException {
+    // validation of first name and last name by using lambda expression.
+    ValidateUsersDetails validateName = name -> {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         try {
             if (name.length() == 0) {
@@ -31,36 +35,46 @@ public class UserValidation {
         } catch (NullPointerException exception) {
             throw new UserValidationException("Please Entered Valid Name");
         }
-    }
-    // Method to validate user mobile number.
-    public String  validateMobileNumber(String mobileNum) throws UserValidationException {
+    };
+
+    // Validation of mobile number by using lambda expression
+    ValidateUsersDetails validateMobileNumber = mobileNumber -> {
         Pattern pattern = Pattern.compile(MOBILE_PATTERN);
         try {
-         if (pattern.matcher(mobileNum).matches()) {
-             return "Valid mobile number.";
-         } else {
-             throw new UserValidationException("Please entered the valid mobile number");
-         }
+            if (pattern.matcher(mobileNumber).matches()) {
+                return "Valid mobile number.";
+            } else {
+                throw new UserValidationException("Please entered the valid mobile number");
+            }
         } catch (NullPointerException exception) {
             throw new UserValidationException("Please entered the valid mobile number address");
         }
-    }
-    // Method validate password
-    public String validatePass(String pass) throws UserValidationException {
+    };
+
+    // validation of Password by using lambda expression.
+    ValidateUsersDetails validatePassword = password -> {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
         try {
-            Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
-            if (!pattern.matcher(pass).matches()) {
+            if (!pattern.matcher(password).matches()) {
                 throw new UserValidationException("Please enter the correct password");
             } else {
-                return "Valid password";
+                return "Valid password.";
             }
         } catch (NullPointerException exception){
             throw new UserValidationException("Please enter the correct password");
         }
-    }
-    // Method to validate user email address
-    public boolean validateEmail(String email) {
+    };
+
+    ValidateUsersDetails validateEmails = emails -> {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        return pattern.matcher(email).matches();
-    }
+        try {
+            if (pattern.matcher(emails).matches()) {
+                throw new UserValidationException("Valid Email");
+            } else {
+                throw new UserValidationException("Please enter the correct email address");
+            }
+        } catch (NullPointerException exception) {
+            throw new UserValidationException("Please enter the correct email address");
+        }
+    };
 }
--- src/test/java/com/userregistration/UserValidationTest.java
+++ src/test/java/com/userregistration/UserValidationTest.java
@@ -9,55 +9,58 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserValidationTest {
     UserValidation validation;
-
     @BeforeEach
     public void setup() {
         validation = new UserValidation();
     }
     @Test
     public void givenFirstName_LastNameWhenProperShouldReturnValidName() throws UserValidationException {
-        String fNameResult = validation.validateName("Chandradip");
-        String lNameResult = validation.validateName("Shivankar");
+        String fNameResult = validation.validateName.validate("Chandradip");
+        String lNameResult = validation.validateName.validate("Shivankar");
         Assertions.assertEquals("Valid Name",fNameResult);
         Assertions.assertEquals("Valid Name",lNameResult);
     }
 
+    @DisplayName("given fist name when proper should return Valid name and when incorrect should throw an exception.")
     @Test
     public void givenFirstName_LastNameWhenShouldThrowAnException() {
         UserValidation userValidation = new UserValidation();
         try {
-            String fName = validation.validateName(null);
+            String fName = validation.validateName.validate(null);
             Assertions.assertEquals("Valid Name", fName);
         } catch (Exception | UserValidationException exception) {
             System.out.println(exception);
         }
     }
+
         @Test
         public void givenMobileNumberWhenProperShouldReturnValidMobileNumber () throws UserValidationException {
-            String mobileNumber = validation.validateMobileNumber("91 9145388092");
+            String mobileNumber = validation.validateMobileNumber.validate("91 9145388092");
             Assertions.assertEquals("Valid mobile number.", mobileNumber);
         }
+    @DisplayName("given mobile number when proper should return Valid mobile number and when incorrect should throw an exception.")
         @Test
         public void givenMobileNumberShouldThrowAnException () {
         try {
-            String mobileNumber = validation.validateMobileNumber("544943569");
+            String mobileNumber = validation.validateMobileNumber.validate("544943569");
             Assertions.assertEquals("Valid mobile number.", mobileNumber);
         } catch (Exception | UserValidationException exception) {
             System.out.println(exception);
         }
         }
+
         @Test
         public void givenPasswordShouldReturnValidPassword () throws UserValidationException {
-            String userPass = validation.validatePass("Dipak@124");
-            Assertions.assertEquals(userPass, "Valid Password.");
+            String userPass = validation.validatePassword.validate("Dipak@124");
+            Assertions.assertEquals(userPass, "Valid password.");
         }
         @Test
-        public void givenNullShouldReturnPleaseEnteredValidPassword () {
+        public void givenPasswordShouldReturnPleaseEnteredValidPassword () {
             String userPass;
             try {
-                userPass = validation.validatePass("5646j");
+                userPass = validation.validatePassword.validate("5646j");
                 Assertions.assertEquals(userPass, "Please entered valid password");
-            } catch (UserValidationException exception) {
+            } catch (Exception | UserValidationException exception) {
                 System.out.println(exception);
             }
         }
@@ -71,9 +74,13 @@ public class UserValidationTest {
                 ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com",
                 "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"})
 
-        public void givenEmailWhenProperShouldReturnTrue (String emailId){
-            boolean emailAddress = validation.validateEmail(emailId);
-            Assertions.assertTrue(emailAddress);
+        public void givenEmailWhenProperShouldReturnValidEmail (String email) {
+            try {
+                String emailAddress = validation.validateEmails.validate(email);
+                Assertions.assertEquals(emailAddress, "Valid Email");
+            } catch (Exception | UserValidationException exception) {
+                System.out.println(exception);
+            }
         }
     }
 
